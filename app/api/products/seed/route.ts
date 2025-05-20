62b75a93f808470e58b05201c22aa0aa108dfa6c
Mubashar217 <muhammadmubasharoffical@gmail.com>
2025-05-20 12:37:43 +0500

    done

---
+++ app/api/products/seed/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import data from '@/lib/data';
+import dbConnect from '@/lib/dbConnect';
+
+export const GET = async (request: NextRequest) => {
+  const { users, products } = data;
+  await dbConnect();
+  
+
+  return NextResponse.json({
+    message: 'seeded successfully',
+    // users,
+    // products,
+  });
+};
